8e3b4babe4e9e371fb755c02a85dd1e80c5c6098
Cory Lueninghoener <cluening@gmail.com>
2020-10-03 15:33:52 +0100

    Initial commit

---
+++ dalek.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import gpiozero
+#from gpiozero import Button
+import os
+import random
+import time
+
+# Notes
+# Set volume: sudo amixer cset numid=1 70%
+
+#button = Button(4, pull_up=False, bounce_time=60)
+#
+#while True:
+#    button.wait_for_press()
+#    print("EXTERMINATE")
+#    os.system("mpg123 /home/pi/halloweendalek/s1_06_aud_03.mp3")
+#    button.wait_for_release()
+
+pir = gpiozero.MotionSensor(pin=4, queue_len=50, sample_rate=10, threshold=0.75)
+
+while True:
+    print("Waiting for motion...")
+    pir.wait_for_motion()
+    randnum = random.randrange(10)
+    if randnum == 1:
+        print("AGHHHH")
+        os.system("mpg123 -q /home/pi/halloweendalek/s1_06_aud_02.mp3")
+    elif randnum in [0, 2, 4, 6, 8]:
+        print("EXTERMINATE")
+        os.system("mpg123 -q /home/pi/halloweendalek/s1_06_aud_03.mp3")
+    else:
+        print("SILENTLY BIDING MY TIME")
+    #print("Sleeping...")
+    #time.sleep(10)
+    #print("Done sleeping.")
+    print("Waiting for motion to stop...")
+    pir.wait_for_no_motion()
