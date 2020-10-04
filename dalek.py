39a651b4f3e34e89f065a2035334d943e8442abe
Cory Lueninghoener <cluening@gmail.com>
2020-10-04 17:22:42 +0100

    Add timestamps; play with the PIR queue length

--- dalek.py
+++ dalek.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import datetime
 import gpiozero
 #from gpiozero import Button
 import os
@@ -17,18 +18,19 @@ import time
 #    os.system("mpg123 /home/pi/halloweendalek/s1_06_aud_03.mp3")
 #    button.wait_for_release()
 
-pir = gpiozero.MotionSensor(pin=4, queue_len=50, sample_rate=10, threshold=0.75)
+pir = gpiozero.MotionSensor(pin=4, queue_len=500, sample_rate=100, threshold=0.90)
 
 while True:
     print("Waiting for motion...")
     pir.wait_for_motion()
+    print(datetime.datetime.now())
     randnum = random.randrange(10)
     if randnum == 1:
         print("AGHHHH")
-        os.system("mpg123 -q /home/pi/halloweendalek/s1_06_aud_02.mp3")
+#        os.system("mpg123 -q /home/pi/halloweendalek/s1_06_aud_02.mp3")
     elif randnum in [0, 2, 4, 6, 8]:
         print("EXTERMINATE")
-        os.system("mpg123 -q /home/pi/halloweendalek/s1_06_aud_03.mp3")
+#        os.system("mpg123 -q /home/pi/halloweendalek/s1_06_aud_03.mp3")
     else:
         print("SILENTLY BIDING MY TIME")
     #print("Sleeping...")
