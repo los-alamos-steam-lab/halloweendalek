697ad294a26a0c589a4741fabda5365618808c02
Lisabeth Lueninghoener <lisjump@gmail.com>
2020-10-05 23:29:03 +0100

    added the camera for reaction pictures

--- dalek.py
+++ dalek.py
@@ -6,17 +6,10 @@ import gpiozero
 import os
 import random
 import time
+import subprocess
 
 # Notes
 # Set volume: sudo amixer cset numid=1 70%
-
-#button = Button(4, pull_up=False, bounce_time=60)
-#
-#while True:
-#    button.wait_for_press()
-#    print("EXTERMINATE")
-#    os.system("mpg123 /home/pi/halloweendalek/s1_06_aud_03.mp3")
-#    button.wait_for_release()
 
 pir = gpiozero.MotionSensor(pin=4, queue_len=500, sample_rate=100, threshold=0.90)
 
@@ -24,7 +17,7 @@ responses = []
 weights = []
 
 class response:
-    def __init__(self, soundfile = None, printstatement = "", weight = 10):
+    def __init__(self, soundfile = None, printstatement = "", weight = 1):
         self.soundfile = soundfile
         self.printstatement = printstatement
         self.weight = weight
@@ -32,8 +25,10 @@ class response:
     def respond(self):
         print(self.printstatement)
         if self.soundfile:
+            subprocess.Popen(["raspistill", "-o", "/home/pi/halloweendalek/" + str(datetime.datetime.now()).replace(" ", "_") + ".jpg"])
+            time.sleep(4.5)
             os.system("mpg123 -q " + self.soundfile)
-
+ 
 exterminate = response(\
     soundfile = "/home/pi/halloweendalek/s1_06_aud_03.mp3", \
     printstatement = "EXTERMINATE", \
@@ -45,17 +40,25 @@ weights.append(exterminate.weight)
 aghhhh = response(\
     soundfile = "/home/pi/halloweendalek/s1_06_aud_02.mp3", \
     printstatement = "AGHHHH", \
-    weight = 10)
+    weight = 1)
 
 responses.append(aghhhh)
 weights.append(aghhhh.weight)
 
 silence = response(\
     printstatement = "SILENTLY BIDING MY TIME", \
-    weight = 10)
+    weight = 0)
 
 responses.append(silence)
 weights.append(silence.weight)
+
+trick = response(\
+    soundfile = "/home/pi/halloweendalek/trick-or-treat.mp3", \
+    printstatement = "Trick or Treat", \
+    weight = .1)
+
+responses.append(trick)
+weights.append(trick.weight)
 
 
 while True:
@@ -64,8 +67,10 @@ while True:
     print(datetime.datetime.now())
     reaction = random.choices(responses, weights)
     reaction[0].respond()
-    #print("Sleeping...")
-    #time.sleep(10)
-    #print("Done sleeping.")
+    
+    print("Sleeping...")
+    time.sleep(10)
+    print("Done sleeping.")
+    
     print("Waiting for motion to stop...")
     pir.wait_for_no_motion()
