ec1334a9c4e241528c8afeddbcce04a6e8b11f64
Lisabeth Lueninghoener <lisjump@gmail.com>
2020-10-05 17:35:59 +0100

    added classes for response

--- dalek.py
+++ dalek.py
@@ -20,19 +20,50 @@ import time
 
 pir = gpiozero.MotionSensor(pin=4, queue_len=500, sample_rate=100, threshold=0.90)
 
+responses = []
+weights = []
+
+class response:
+    def __init__(self, soundfile = None, printstatement = "", weight = 10):
+        self.soundfile = soundfile
+        self.printstatement = printstatement
+        self.weight = weight
+
+    def respond(self):
+        print(self.printstatement)
+        if self.soundfile:
+            os.system("mpg123 -q " + self.soundfile)
+
+exterminate = response(\
+    soundfile = "/home/pi/halloweendalek/s1_06_aud_03.mp3", \
+    printstatement = "EXTERMINATE", \
+    weight = 10)
+
+responses.append(exterminate)
+weights.append(exterminate.weight)
+
+aghhhh = response(\
+    soundfile = "/home/pi/halloweendalek/s1_06_aud_02.mp3", \
+    printstatement = "AGHHHH", \
+    weight = 10)
+
+responses.append(aghhhh)
+weights.append(aghhhh.weight)
+
+silence = response(\
+    printstatement = "SILENTLY BIDING MY TIME", \
+    weight = 10)
+
+responses.append(silence)
+weights.append(silence.weight)
+
+
 while True:
     print("Waiting for motion...")
     pir.wait_for_motion()
     print(datetime.datetime.now())
-    randnum = random.randrange(10)
-    if randnum == 1:
-        print("AGHHHH")
-#        os.system("mpg123 -q /home/pi/halloweendalek/s1_06_aud_02.mp3")
-    elif randnum in [0, 2, 4, 6, 8]:
-        print("EXTERMINATE")
-#        os.system("mpg123 -q /home/pi/halloweendalek/s1_06_aud_03.mp3")
-    else:
-        print("SILENTLY BIDING MY TIME")
+    reaction = random.choices(responses, weights)
+    reaction[0].respond()
     #print("Sleeping...")
     #time.sleep(10)
     #print("Done sleeping.")
